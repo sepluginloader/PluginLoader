e8b27af782f4a342933769cf75adc29838abca88
austinvaness <austinvaness@gmail.com>
2021-03-06 10:43:33 -0600

    Moved menu button.

--- PluginLoader/Patch/Patch_CreateMenu.cs
+++ PluginLoader/Patch/Patch_CreateMenu.cs
@@ -1,32 +1,62 @@
 ﻿using HarmonyLib;
-using Sandbox.Graphics;
 using Sandbox.Graphics.GUI;
 using SpaceEngineers.Game.GUI;
 using System.Text;
+using VRage.Game;
 using VRage.Utils;
 using VRageMath;
 
 namespace avaness.PluginLoader.Patch
 {
-    [HarmonyPatch(typeof(MyGuiScreenMainMenu), "RecreateControls")]
-    public static class Patch_CreateMenu
-    {
-        public static void Postfix(MyGuiScreenMainMenu __instance)
-        {
-            __instance.AddControl(
-                new MyGuiControlButton(MyGuiManager.ComputeFullscreenGuiCoordinate(MyGuiDrawAlignEnum.HORISONTAL_RIGHT_AND_VERTICAL_TOP, 0, 0), originAlign: MyGuiDrawAlignEnum.HORISONTAL_RIGHT_AND_VERTICAL_TOP, text: new StringBuilder("Plugins"), onButtonClick: OpenMenu, toolTip: "Click to configure plugins.")
+    [HarmonyPatch(typeof(MyGuiScreenMainMenu), "CreateMainMenu")]
+	public static class Patch_CreateMainMenu
+	{
+		public static void Postfix(MyGuiScreenMainMenu __instance, Vector2 leftButtonPositionOrigin, ref Vector2 lastButtonPosition)
+		{
+			MyGuiControlButton lastBtn = null;
+			foreach(var control in __instance.Controls)
+            {
+				if (control is MyGuiControlButton btn && btn.Position == lastButtonPosition)
                 {
-                    BorderEnabled = true,
-                    BorderSize = 1,
-                    BorderHighlightEnabled = true,
-                    BorderColor = Vector4.Zero
-                });
+					lastBtn = btn;
+					break;
+				}
+			}
 
-        }
+			Vector2 position;
+            if (lastBtn == null)
+            {
+                position = lastButtonPosition + MyGuiConstants.MENU_BUTTONS_POSITION_DELTA;
+            }
+            else
+            {
+                position = lastBtn.Position;
+                lastBtn.Position = lastButtonPosition + MyGuiConstants.MENU_BUTTONS_POSITION_DELTA;
+            }
 
-        private static void OpenMenu(MyGuiControlButton btn)
-        {
-            MyGuiSandbox.AddScreen(new MyGuiScreenPluginConfig());
-        }
-    }
+            MyGuiControlButton openBtn = new MyGuiControlButton(position, MyGuiControlButtonStyleEnum.StripeLeft, originAlign: MyGuiDrawAlignEnum.HORISONTAL_CENTER_AND_VERTICAL_BOTTOM, text: new StringBuilder("Plugins"), onButtonClick: OpenMenu)
+			{
+				BorderEnabled = false,
+				BorderSize = 0,
+				BorderHighlightEnabled = false,
+				BorderColor = Vector4.Zero
+			};
+			__instance.Controls.Add(openBtn);
+		}
+
+		private static void OpenMenu(MyGuiControlButton btn)
+		{
+			MyGuiSandbox.AddScreen(new MyGuiScreenPluginConfig());
+		}
+	}
+
+
+	[HarmonyPatch(typeof(MyGuiScreenMainMenu), "CreateInGameMenu")]
+	public static class Patch_CreateInGameMenu
+	{
+		public static void Postfix(MyGuiScreenMainMenu __instance, Vector2 leftButtonPositionOrigin, ref Vector2 lastButtonPosition)
+		{
+			Patch_CreateMainMenu.Postfix(__instance, leftButtonPositionOrigin, ref lastButtonPosition);
+		}
+	}
 }
