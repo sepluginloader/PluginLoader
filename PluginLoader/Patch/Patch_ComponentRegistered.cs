1d7d2633e5a42e69a8e974ed1dc6f0f4a61bdb4b
austinvaness <austinvaness@gmail.com>
2021-03-06 10:44:08 -0600

    Fixed order of session component registration

--- PluginLoader/Patch/Patch_ComponentRegistered.cs
+++ PluginLoader/Patch/Patch_ComponentRegistered.cs
@@ -1,14 +1,19 @@
 ﻿using HarmonyLib;
 using Sandbox.Game.World;
+using System.Reflection;
+using VRage.Game;
+using VRage.Plugins;
 
 namespace avaness.PluginLoader.Patch
 {
-    [HarmonyPatch(typeof(MySession), "RegisterComponentsFromAssemblies")]
+    [HarmonyPatch(typeof(MySession), "RegisterComponentsFromAssembly")]
+    [HarmonyPatch(new[] { typeof(Assembly), typeof(bool), typeof(MyModContext) })]
     public static class Patch_ComponentRegistered
     {
-        public static void Postfix()
+        public static void Prefix(Assembly assembly)
         {
-            Main.Instance?.RegisterComponents();
+            if(assembly == MyPlugins.GameAssembly)
+                Main.Instance?.RegisterComponents();
         }
     }
 }
