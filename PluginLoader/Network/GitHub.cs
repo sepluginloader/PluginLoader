f86f605831be5578c7ad66b92403cba82dbe0f41
austinvaness <austinvaness@gmail.com>
2021-03-16 20:51:17 -0500

    Basic networking

---
+++ PluginLoader/Network/GitHub.cs
@@ -0,0 +1,42 @@
+﻿using LitJson;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace avaness.PluginLoader.Network
+{
+    public static class GitHub
+    {
+        private const string githubApi = "https://api.github.com/repos/austinvaness/PluginLoader/";
+
+        private static JsonData GetResponse(string path)
+        {
+            Uri uri = new Uri(githubApi + path, UriKind.Absolute);
+            HttpWebRequest request = WebRequest.CreateHttp(uri);
+            request.UserAgent = "Space-Engineers-Plugin-Loader";
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return JsonMapper.ToObject(reader);
+            }
+        }
+
+        public static IEnumerable<GitHubFile> GetFiles(string path)
+        {
+            JsonData json = GetResponse("contents/" + path.TrimStart('/'));
+            if (json.IsArray)
+            {
+                GitHubFile temp;
+                foreach(JsonData fileJson in json)
+                {
+                    if (GitHubFile.TryGet(fileJson, out temp))
+                        yield return temp;
+                }
+            }
+        }
+    }
+}
---
+++ PluginLoader/Network/GitHubFile.cs
@@ -0,0 +1,44 @@
+﻿using LitJson;
+using System;
+
+namespace avaness.PluginLoader.Network
+{
+    public class GitHubFile
+    {
+        public string Name { get; }
+        public Uri DownloadUrl { get; }
+        public bool IsDirectory { get; }
+
+        public GitHubFile(string name, Uri downloadUrl, bool isDirectory)
+        {
+            Name = name;
+            DownloadUrl = downloadUrl;
+            IsDirectory = isDirectory;
+        }
+
+        public static bool TryGet(JsonData data, out GitHubFile file)
+        {
+            file = null;
+            if(data.IsObject && data.ContainsKey("name") && data.ContainsKey("download_url") && data.ContainsKey("type"))
+            {
+                JsonData name = data["name"];
+                if (name == null || !name.IsString)
+                    return false;
+
+                JsonData type = data["type"];
+                if (type == null || !type.IsString)
+                    return false;
+
+                JsonData downloadUrl = data["download_url"];
+                Uri uri = null;
+                if (downloadUrl != null && downloadUrl.IsString && !Uri.TryCreate((string)downloadUrl, UriKind.Absolute, out uri))
+                    return false;
+
+                file = new GitHubFile((string)name, uri, (string)type != "dir");
+                return true;
+
+            }
+            return false;
+        }
+    }
+}
