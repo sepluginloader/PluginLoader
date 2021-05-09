a37f2ff5a574771fd5dd597903bfc2e43d51b435
austinvaness <austinvaness@gmail.com>
2021-05-09 11:33:45 -0500

    Fix local plugin dependencies

--- PluginLoader/Data/LocalPlugin.cs
+++ PluginLoader/Data/LocalPlugin.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Reflection;
 using VRage;
@@ -39,6 +40,7 @@ namespace avaness.PluginLoader.Data
         {
             if(File.Exists(Id))
             {
+                AppDomain.CurrentDomain.AssemblyResolve += LoadFromSameFolder;
                 Assembly a = Assembly.LoadFile(Id);
                 Version = a.GetName().Version;
                 return a;
@@ -56,6 +58,28 @@ namespace avaness.PluginLoader.Data
             string file = Path.GetFullPath(Id);
             if (File.Exists(file))
                 Process.Start("explorer.exe", $"/select, \"{file}\"");
+        }
+
+        private Assembly LoadFromSameFolder(object sender, ResolveEventArgs args)
+        {
+            if (args.RequestingAssembly.IsDynamic)
+                return null;
+
+            if (args.Name.Contains("0Harmony") || args.Name.Contains("SEPluginManager"))
+                return null;
+
+            string location = args.RequestingAssembly.Location;
+            if (string.IsNullOrWhiteSpace(location) || !Path.GetFullPath(location).StartsWith(Path.GetDirectoryName(Id), StringComparison.OrdinalIgnoreCase))
+                return null;
+
+            string folderPath = Path.GetDirectoryName(location);
+            string assemblyPath = Path.Combine(folderPath, new AssemblyName(args.Name).Name + ".dll");
+            if (!File.Exists(assemblyPath))
+                return null;
+
+            Assembly assembly = Assembly.LoadFile(assemblyPath);
+            LogFile.WriteLine("Resolving " + assembly.GetName().Name + " for " + args.RequestingAssembly.FullName);
+            return assembly;
         }
     }
 }
