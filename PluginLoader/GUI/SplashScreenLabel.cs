a53608ca838f0a80055cbb3aed0b7189a665c907
WesternGamer <Kevnguyen474@gmail.com>
2021-08-11 22:13:04 -0400

    Reverted changes as it is hard to make loading wheel

--- PluginLoader/GUI/SplashScreenLabel.cs
+++ PluginLoader/GUI/SplashScreenLabel.cs
@@ -33,6 +33,7 @@ namespace avaness.PluginLoader.GUI
             lbl.ForeColor = System.Drawing.Color.White;
             lbl.Font = new Font(FontFamily.GenericSansSerif, 25, FontStyle.Regular);
             form.Controls.Add(lbl);
+            
         }
         
         public void SetText(string msg)
@@ -50,44 +51,5 @@ namespace avaness.PluginLoader.GUI
             form.Controls.Remove(lbl);
         }
 
-        /// <summary>
-        /// Method to rotate an image either clockwise or counter-clockwise
-        /// </summary>
-        /// <param name="img">the image to be rotated</param>
-        /// <param name="rotationAngle">the angle (in degrees).
-        /// NOTE: 
-        /// Positive values will rotate clockwise
-        /// negative values will rotate counter-clockwise
-        /// </param>
-        /// <returns></returns>
-        public static Image RotateImage(Image img, float rotationAngle)
-        {
-            //create an empty Bitmap image
-            Bitmap bmp = new Bitmap(img.Width, img.Height);
-
-            //turn the Bitmap into a Graphics object
-            Graphics gfx = Graphics.FromImage(bmp);
-
-            //now we set the rotation point to the center of our image
-            gfx.TranslateTransform((float)bmp.Width / 2, (float)bmp.Height / 2);
-
-            //now rotate the image
-            gfx.RotateTransform(rotationAngle);
-
-            gfx.TranslateTransform(-(float)bmp.Width / 2, -(float)bmp.Height / 2);
-
-            //set the InterpolationMode to HighQualityBicubic so to ensure a high
-            //quality image once it is transformed to the specified size
-            gfx.InterpolationMode = InterpolationMode.HighQualityBicubic;
-
-            //now draw our new image onto the graphics object
-            gfx.DrawImage(img, new Point(0, 0));
-
-            //dispose of our Graphics object
-            gfx.Dispose();
-
-            //return the image
-            return bmp;
-        }
     }
 }
--- PluginLoader/Properties/Resources.Designer.cs
+++ PluginLoader/Properties/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace avaness.PluginLoader.Properties {
                 return ((byte[])(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap LoadingWheel {
+            get {
+                object obj = ResourceManager.GetObject("LoadingWheel", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
