0bcaebf3b7566e538e60d04703765c38c9aa0ed8
austinvaness <austinvaness@gmail.com>
2022-06-06 11:58:29 -0500

    Revert "Updating for my own install location"
    This reverts commit 04b4fc6787f11a7699cfcb18bf703663f309711d.

--- Edit-and-run-before-opening-solution.bat
+++ Edit-and-run-before-opening-solution.bat
@@ -1,9 +1,9 @@
 @echo off
 
 REM Location of your SpaceEngineers.exe
-mklink /J Bin64 "H:\SteamLibrary\steamapps\common\SpaceEngineers\Bin64"
+mklink /J Bin64 "C:\Program Files (x86)\Steam\steamapps\common\SpaceEngineers\Bin64"
 
 REM Location of your workshop
-mklink /J workshop "H:\SteamLibrary\steamapps\workshop"
+mklink /J workshop "C:\Program Files (x86)\Steam\steamapps\workshop"
 
 pause
