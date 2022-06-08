cb50979a1e83e531483fcec0d7f7d39c86a50971
austinvaness <45703294+austinvaness@users.noreply.github.com>
2022-06-08 13:56:26 -0500

    Fix RunPluginLoader deploy.bat

--- RunPluginLoader/deploy.bat
+++ RunPluginLoader/deploy.bat
@@ -3,13 +3,13 @@ Rem Run this script with the full file path as argument
 
 if [%1] == [] goto eof
 
-set workshop="%~dp0..\workshop\content\244850\2407984968\%~n1"
+set workshop="%~dp0..\workshop\content\244850\2407984968"
 set counter=1
 
 Rem Wait for the file to be ready
 :waitfile
 2>nul (
- >>%workshop% (call )
+ >>%workshop%\%~n1 (call )
 ) && (goto copyfile) || (echo File is in use.)
 set /a counter=counter+1
 echo Trying attempt #%counter%
@@ -19,7 +19,7 @@ goto waitfile
 Rem Copy the file to the target location
 :copyfile
 echo Copying file.
-copy /y "%~1" %workshop%
+copy /y "%~1" %workshop%\%~n1
 copy /y "%~1" "%appdata%\SpaceEngineers\Mods\Plugin Loader\%~n1"
 
 :eof
