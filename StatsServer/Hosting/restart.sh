f253497ff52250f5916dd4c8f102d7d3d71a76db
avaness <austinvaness@gmail.com>
2023-04-10 21:23:30 -0500

    Move StatsServer
    StatsServer moved to its own repository

--- StatsServer/Hosting/restart.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-bash ~/bin/stop.sh
-bash ~/bin/start.sh
--- StatsServer/Hosting/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-. $HOME/bin/env.sh
-
-TODAY=$(date -I)
-
-cd "$PL_SERVER_DIR"
-dotnet StatsServer.dll >>"${PL_LOG_DIR}/${TODAY}.jsonl" 2>&1 &
