1972d0d6cf3ba1adf2f4c980324aded1c377d1c2
avaness <austinvaness@gmail.com>
2023-05-12 23:52:30 -0500

    Fix dynamic type in plugins

--- PluginLoader/Compiler/RoslynReferences.cs
+++ PluginLoader/Compiler/RoslynReferences.cs
@@ -45,6 +45,11 @@ namespace avaness.PluginLoader.Compiler
                     AddAssemblyReference(a);
                     sb.AppendLine(a.FullName);
                 }
+                foreach(Assembly a in GetOtherReferences())
+                {
+                    AddAssemblyReference(a);
+                    sb.AppendLine(a.FullName);
+                }
                 sb.AppendLine(line);
                 while (loadedAssemblies.Count > 0)
                 {
@@ -75,6 +80,14 @@ namespace avaness.PluginLoader.Compiler
             }
 
             LogFile.WriteLine(sb.ToString(), false);
+        }
+
+        /// <summary>
+        /// This method is used to load references that otherwise would not exist or be optimized out
+        /// </summary>
+        private static IEnumerable<Assembly> GetOtherReferences()
+        {
+            yield return typeof(Microsoft.CSharp.RuntimeBinder.Binder).Assembly;
         }
 
         private static bool ContainsReference(AssemblyName name)
